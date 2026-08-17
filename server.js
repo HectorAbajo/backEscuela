85a7bad7016d293d5800e3d6d3fceaf40d8be479
HectorAbajo <110790345+HectorAbajo@users.noreply.github.com>
2026-08-17 13:01:19 -0300

    Integrate clases router into main server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ dotenv.config()
 
 const { authRouter } = require('./1autentificacion/aut.router')
 const { adminRouter } = require('./2administrativo/admin.router.js')
+const { clasesRouter } = require('./2administrativo/clases.router.js')
 const { alumnRouter } = require('./3alumnos/alumn.router.js')
 const { profRouter } = require("./4profesores/prof.router.js")
 
@@ -22,6 +23,7 @@ app.use(express.json())
 
 app.use('/api/auth', authRouter)
 app.use('/api/admin', adminRouter)
+app.use('/api/clases', clasesRouter)
 app.use('/api/alumno', alumnRouter)
 app.use('/api/profesor', profRouter)
 
